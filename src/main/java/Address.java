8e5cbdfe49550210cad7af7ce7df5dcc1b9a3905
Ricardo <ricardocampox@hotmail.com>
2019-11-20 19:34:12 +0000

    Criação da Classe Address

---
+++ src/main/java/Address.java
@@ -0,0 +1,31 @@
+public class Address
+{
+    private  String streetName;
+    private  String doorNo;
+    private  String postalCode;
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public String getDoorNo() {
+        return doorNo;
+    }
+
+    public void setDoorNo(String doorNo) {
+        this.doorNo = doorNo;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+}
+
