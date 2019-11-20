4c0b0236131dcc1662fe5e20cd9c5c32fcd50a35
Ricardo <ricardocampox@hotmail.com>
2019-11-20 20:18:26 +0000

    Criação da Classe User

---
+++ src/main/java/User.java
@@ -0,0 +1,52 @@
+public class User
+{
+    private String name;
+    private String email;
+    private String username;
+    private String password;
+    private Address adress;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Address getAdress() {
+        return adress;
+    }
+
+    public void setAdress(Address adress) {
+        this.adress = adress;
+    }
+    public String getStreetName(){
+        return this.adress.getStreetName();
+    }
+}
